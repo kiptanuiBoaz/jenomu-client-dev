f778a844c7c4e10d415c1a20f39165fdcc7a881d
Boaz Serem <boaserem022@gmail.com>
2024-06-10 12:17:27 +0300

    feat: add MUI theming  && git push

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,16 @@
 import { Provider } from 'react-redux';
 import store from './redux/store/store';
 import AppRoutes from './config/routes';
+import { theme } from './theme/theme';
+import { ThemeProvider } from '@mui/material';
 
 const App = () => {
   return (
     <Provider store={store}>
-      <AppRoutes />
+      <ThemeProvider theme={theme}>
+        <AppRoutes />
+      </ThemeProvider>
+
     </Provider>
   );
 };
---
+++ src/theme/theme.ts
@@ -0,0 +1,17 @@
+import { createTheme } from "@mui/material";
+
+export const theme = createTheme({
+    palette: {
+        primary: {
+            main: '#228B22',
+        },
+        secondary: {
+            main: '#f50057',
+        },
+    },
+    typography: {
+        fontFamily: 'Arial',
+        fontSize: 16,
+
+    },
+}); 
